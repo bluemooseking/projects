3a7909e32a3383e43a54c96dfe0193141997e262
jhills <jhills@diablo-technologies.com>
2018-04-03 11:28:39 -0400

    add logging capability

--- D2.0/vmm.py
+++ D2.0/vmm.py
@@ -5,7 +5,12 @@ Created on Thu Mar 29 16:23:54 2018
 @author: jhills
 """
 
+import logging, sys
+
 class VMM:
+    # Debug Attributes
+    logger = None
+    
     # Physical Attributes
     PHYS_MEM_SIZE = None
     PHYS_BAK_SIZE = None
@@ -26,25 +31,40 @@ class VMM:
             phys_addr = addr
             phys_mode = mode
             
-    def __init__(self, phys_mem_size, phys_bak_size):
+    def __init__(self, phys_mem_size, phys_bak_size, 
+                 loglevel = logging.ERROR):
+        # Init Debug
+        self.logger = logging.getLogger('vmm')
+        self.logger.setLevel(loglevel)
+        self.logger.debug("Welcome to VMM logger")
+        
+        # Init Physical
         phys_all_size = phys_mem_size + phys_bak_size
         self.PHYS_MEM_SIZE = phys_mem_size
         self.PHYS_BAK_SIZE = phys_bak_size
         self.VIRT_MEM_SIZE = phys_all_size
         
+        # Init Stete Vars
         self.CURRENT_ALLOC_SIZE = 0
         
+        # Init Data structs
         self.VIRT_ADDR_TABLE = dict.fromkeys(range(phys_all_size), 0)
         self.PHYS_MEM = [0] * phys_mem_size
         self.PHYS_BAK = [0] * phys_bak_size
-        
-    def alloc_all(self, size):
+      
+    def get_alloc(self):
+        self.logger.debug("current alloc = %d", self.CURRENT_ALLOC_SIZE)
+        return self.CURRENT_ALLOC_SIZE
+    
+    def alloc_all(self):
         
         # Only do this once
         if self.CURRENT_ALLOC_SIZE != 0:
+            self.logger.warning("alloc all: vmm not empty")
             return -1
         
         # Set it up
+        self.logger.debug("alloc all = %d", self.CURRENT_ALLOC_SIZE)
         self.CURRENT_ALLOC_SIZE = self.VIRT_MEM_SIZE
             
         return 0
@@ -54,5 +74,9 @@ class VMM:
     #def write(self, virt_addr, val):
         
         
-vmm = VMM(1000, 10000)
+vmm = VMM(1000, 10000, loglevel = logging.DEBUG)
+vmm.get_alloc()
+vmm.alloc_all()
+vmm.alloc_all()
+vmm.get_alloc()
  
