df6e8d899eefb98ebc955b2c93ac30b0fd1b816a
jhills <jhills@diablo-technologies.com>
2018-04-03 20:59:14 -0400

    new testbench: basic binary tree

--- D2.0/testbench.py
+++ D2.0/testbench.py
@@ -31,4 +31,28 @@ def gen_sequential_array_faults(filename = None):
             logging.critical("SEQ VALIDATION FAILED")
     logging.critical("SEQ VALIDATION PASSED")
     
-gen_sequential_array_faults('faults_seq.csv')
+def gen_binary_tree_faults_A(filename = None):
+    if filename == None:
+        logging.error("Specify filename")
+    
+    tree_depth = 15
+    tree_size = 2 ** tree_depth
+    tree_mask = 2 ** (tree_depth - 2)
+    m_ratio = 0.01
+    m_loops = 5
+    
+    m_size = int(tree_size * m_ratio)
+    b_size = tree_size - m_size
+    vmm = VMM(m_size, b_size, fault_file = filename)
+    vmm.alloc_all()
+    for i in range(m_loops):
+        for j in range(tree_size):
+            vaddr = 0
+            for k in range(tree_depth):
+                vmm.read(vaddr)
+                vaddr *= 2
+                vaddr += 2 if ((j << k) & tree_mask) else 1
+                
+gen_binary_tree_faults_A('faults_treeA.csv')          
+            
+            
