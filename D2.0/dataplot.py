9ba6dbf17a6377322970d23621c9be98e8243d07
jhills <jhills@diablo-technologies.com>
2018-04-03 23:07:01 -0400

    change cumulation to stackplot

--- D2.0/dataplot.py
+++ D2.0/dataplot.py
@@ -4,7 +4,7 @@ Created on Tue Apr  3 21:16:29 2018
 
 @author: jhills
 """
-
+import numpy as np
 import logging
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,12 +12,19 @@ import matplotlib.pyplot as plt
 def vaddr_histogram(filename, data):
     data['vaddr'].hist(bins = 100)
     plt.title('Vaddr Histogram: %s' % (filename))
+    plt.xlabel('vaddr')
+    plt.ylabel('hit count')
     plt.show()
 
 def fault_type_cumulative(filename, data):
-    onehot = pd.get_dummies(data['fault_type'])
-    onehot.cumsum().plot()
+    one_hot = pd.get_dummies(data['fault_type'])
+    cum_sum = np.array(one_hot).cumsum(axis=0)
+    plt.stackplot(np.array(range(len(one_hot))), 
+                  (cum_sum/cum_sum.sum(axis=1, keepdims=True)).T, 
+                  labels = one_hot.columns)
     plt.title('Fault Types (Cumulative): %s' % (filename))
+    plt.xlabel('fault count')
+    plt.legend(loc = 2)
     plt.show()
     
 def run_visuals(filename = None):
