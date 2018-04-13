44d30b514d613791173c6eca1b35788604485c7f
jhills <jhills@diablo-technologies.com>
2018-04-13 19:37:40 -0400

    add random transate to bintree test

--- D2.0/testbench.py
+++ D2.0/testbench.py
@@ -10,7 +10,9 @@ import random
 from vmm import VMM
 
 def test_sequential_array(filename = None,
-                          pred_def = None):   
+                          pred_def = None,
+                          test_def = None,
+                          ):   
     data_size = 1000
     m_ratio = 0.1
     m_loops = 100
@@ -29,13 +31,26 @@ def test_sequential_array(filename = None,
     return (True, vmm)
     
 def test_binary_tree(filename = None,
-                     pred_def = None):    
+                     pred_def = None,
+                     test_def = None,
+                     ):
+      
+    def _get_translate_table(depth):
+        order = list(range(2 ** depth))
+        random.shuffle(order)
+        return order       
+        
     tree_depth = 13
     tree_size = 2 ** tree_depth
     tree_mask = 2 ** (tree_depth - 2)
     m_ratio = 0.01
     m_loops = 5
     
+    if ((test_def) and ('rand_translate' in test_def) and (test_def['rand_translate'])):
+        vaddr_translate = _get_translate_table(tree_depth)
+    else:
+        vaddr_translate = list(range(2 ** tree_depth))
+        
     m_size = int(tree_size * m_ratio)
     b_size = tree_size - m_size
     vmm = VMM(m_size, b_size, fault_file = filename, pred_def = pred_def)
@@ -44,16 +59,16 @@ def test_binary_tree(filename = None,
         for j in range(tree_size):
             vaddr = 0
             for k in range(tree_depth):
-                mem[vaddr]
+                mem[vaddr_translate[vaddr]]
                 vaddr *= 2
                 vaddr += 2 if ((j << k) & tree_mask) else 1
     return (True, vmm)
     
-def run_test(name = None, testFunc = None,
+def run_test(name = None, testFunc = None, test_def = None,
              faultFile = None, pred_def = None):
     logging.critical("==============================================")
     logging.critical("%s BEGIN" % (name))
-    ret, vmm = testFunc(faultFile, pred_def)
+    ret, vmm = testFunc(faultFile, pred_def, test_def)
     if (ret):
         logging.critical("%s PASSED" % (name))
     else:
@@ -63,19 +78,19 @@ def run_test(name = None, testFunc = None,
     for kvp in vmm.get_counter_values():
         logging.critical("%30s: %d" % kvp)
     
-
-
 PREDS = [('P0-NONE',  None), 
          ('P1-SEQ',   {'mode' : 'seq'}),
          ('P2-HIST',  {'mode' : 'hist'}),
         ]
-TESTS = [('T0-SEQ', test_sequential_array),
-         ('T1-BIN', test_binary_tree),
+TESTS = [('T0-SEQ', test_sequential_array, None),
+         ('T1-BIN', test_binary_tree, None),
+         ('T2-BRND', test_binary_tree, {'rand_translate' : True}),
          ]
 for (pname, pdef) in PREDS:
-    for (tname, tdef) in TESTS:
+    for (tname, tfunc, tdef) in TESTS:
         run_test(name = "%s %s" % (tname, pname),
-                 testFunc = tdef,
+                 testFunc = tfunc,
+                 test_def = tdef,
                  faultFile = "vmm.%s.%s.csv" % (tname, pname),
                  pred_def = pdef)
       
